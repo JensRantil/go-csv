db9a5ab941c5f66fa3ee9e742a849f5d8a6a6043
Jens Rantil <jens.rantil@gmail.com>
2014-04-20 08:55:46 +0200

    chore: formatting
     * Used `gofmt` for this.
     * No logic modified.

--- csv.go
+++ csv.go
@@ -3,246 +3,246 @@
 
 // A CSV implementation inspired by Python's CSV module. Supports custom CSV
 // formats. Currently only writing CSV files is supported.
-package csv;
+package csv
 
 import (
-  "bufio"
-  "io"
-  "strings"
+	"bufio"
+	"io"
+	"strings"
 )
 
 // A helper interface for a general CSV writer. Adheres to encoding/csv Writer
 // in the standard go library as well as the Writer implemented by this
 // package.
 type CsvWriter interface {
-  // Currently no errors are possible.
-  Error() error
+	// Currently no errors are possible.
+	Error() error
 
-  // Flush writes any buffered data to the underlying io.Writer.
-  // To check if an error occurred during the Flush, call Error.
-  Flush()
+	// Flush writes any buffered data to the underlying io.Writer.
+	// To check if an error occurred during the Flush, call Error.
+	Flush()
 
-  // Writer writes a single CSV record to w along with any necessary quoting.
-  // A record is a slice of strings with each string being one field.
-  Write(record []string) error
+	// Writer writes a single CSV record to w along with any necessary quoting.
+	// A record is a slice of strings with each string being one field.
+	Write(record []string) error
 
-  // WriteAll writes multiple CSV records to w using Write and then calls Flush.
-  WriteAll(records [][]string) error
+	// WriteAll writes multiple CSV records to w using Write and then calls Flush.
+	WriteAll(records [][]string) error
 }
 
 // Values Dialect.Quoting can take.
 const (
-  QuoteDefault = iota  // See DefaultQuoting.
-  QuoteAll = iota
-  QuoteMinimal = iota
-  QuoteNonNumeric = iota
-  QuoteNone = iota
+	QuoteDefault    = iota // See DefaultQuoting.
+	QuoteAll        = iota
+	QuoteMinimal    = iota
+	QuoteNonNumeric = iota
+	QuoteNone       = iota
 )
 
 // Values Dialect.DoubleQuote can take.
 const (
-  DoubleQuoteDefault = iota  // See DefaultDoubleQuote.
-  DoDoubleQuote = iota
-  NoDoubleQuote = iota
+	DoubleQuoteDefault = iota // See DefaultDoubleQuote.
+	DoDoubleQuote      = iota
+	NoDoubleQuote      = iota
 )
 
 // Default dialect.
 const (
-  DefaultDelimiter = " "
-  DefaultQuoting = QuoteMinimal
-  DefaultDoubleQuote = DoDoubleQuote
-  DefaultEscapeChar = '\\'
-  DefaultQuoteChar = '"'
-  DefaultLineTerminator = "\n"
+	DefaultDelimiter      = " "
+	DefaultQuoting        = QuoteMinimal
+	DefaultDoubleQuote    = DoDoubleQuote
+	DefaultEscapeChar     = '\\'
+	DefaultQuoteChar      = '"'
+	DefaultLineTerminator = "\n"
 )
 
 type Dialect struct {
-  Delimiter string
-  Quoting int
-  DoubleQuote int
-  EscapeChar rune
-  QuoteChar rune
-  LineTerminator string
+	Delimiter      string
+	Quoting        int
+	DoubleQuote    int
+	EscapeChar     rune
+	QuoteChar      rune
+	LineTerminator string
 }
 
 func (wo *Dialect) setDefaults() {
-  if wo.Delimiter == "" {
-    wo.Delimiter = DefaultDelimiter
-  }
-  if wo.Quoting == QuoteDefault {
-    wo.Quoting = DefaultQuoting
-  }
-  if wo.LineTerminator == "" {
-    wo.LineTerminator = DefaultLineTerminator
-  }
-  if wo.DoubleQuote == DoubleQuoteDefault {
-    wo.DoubleQuote = DefaultDoubleQuote
-  }
-  if wo.QuoteChar == 0 {
-    wo.QuoteChar = DefaultQuoteChar
-  }
-  if wo.EscapeChar == 0 {
-    wo.EscapeChar = DefaultEscapeChar
-  }
+	if wo.Delimiter == "" {
+		wo.Delimiter = DefaultDelimiter
+	}
+	if wo.Quoting == QuoteDefault {
+		wo.Quoting = DefaultQuoting
+	}
+	if wo.LineTerminator == "" {
+		wo.LineTerminator = DefaultLineTerminator
+	}
+	if wo.DoubleQuote == DoubleQuoteDefault {
+		wo.DoubleQuote = DefaultDoubleQuote
+	}
+	if wo.QuoteChar == 0 {
+		wo.QuoteChar = DefaultQuoteChar
+	}
+	if wo.EscapeChar == 0 {
+		wo.EscapeChar = DefaultEscapeChar
+	}
 }
 
 type Writer struct {
-  opts Dialect
-  w *bufio.Writer
+	opts Dialect
+	w    *bufio.Writer
 }
 
 // Create a writer that adheres to the Golang CSV writer.
 func NewWriter(w io.Writer) Writer {
-  opts := Dialect{}
-  opts.setDefaults()
-  return Writer{
-    opts: opts,
-    w: bufio.NewWriter(w),
-  }
+	opts := Dialect{}
+	opts.setDefaults()
+	return Writer{
+		opts: opts,
+		w:    bufio.NewWriter(w),
+	}
 }
 
 // Create a custom CSV writer.
 func NewDialectWriter(w io.Writer, opts Dialect) Writer {
-  opts.setDefaults()
-  return Writer{
-    opts: opts,
-    w: bufio.NewWriter(w),
-  }
+	opts.setDefaults()
+	return Writer{
+		opts: opts,
+		w:    bufio.NewWriter(w),
+	}
 }
 
 // Error reports any error that has occurred during a previous Write or Flush.
 func (w Writer) Error() error {
-  _, err := w.w.Write(nil)
-  return err
+	_, err := w.w.Write(nil)
+	return err
 }
 
 // Flush writes any buffered data to the underlying io.Writer.
 // To check if an error occurred during the Flush, call Error.
 func (w Writer) Flush() {
-  w.w.Flush()
+	w.w.Flush()
 }
 
 // Helper function that ditches the first return value of w.w.WriteString().
 // Simplifies code.
 func (w Writer) writeString(s string) error {
-  _, err := w.w.WriteString(s)
-  return err
+	_, err := w.w.WriteString(s)
+	return err
 }
 
 func (w Writer) writeDelimiter() error {
-  return w.writeString(w.opts.Delimiter)
+	return w.writeString(w.opts.Delimiter)
 }
 
 func isDigit(s rune) bool {
-  return s >= '0' && s <= '9'
+	return s >= '0' && s <= '9'
 }
 
 func isNumeric(s string) bool {
-  if len(s) == 0 {
-    return false
-  }
-  for _, r := range s {
-    if !isDigit(r) {
-      return false
-    }
-  }
-  return true
+	if len(s) == 0 {
+		return false
+	}
+	for _, r := range s {
+		if !isDigit(r) {
+			return false
+		}
+	}
+	return true
 }
 
 func (w Writer) fieldNeedsQuote(field string) bool {
-  switch w.opts.Quoting {
-  case QuoteNone:
-    return false
-  case QuoteAll:
-    return true
-  case QuoteNonNumeric:
-    return !isNumeric(field)
-  case QuoteMinimal:
-    // TODO: Can be improved by making a single search with trie.
-    // See https://docs.python.org/2/library/csv.html#csv.QUOTE_MINIMAL for info on this.
-    return strings.Contains(field, w.opts.LineTerminator) || strings.Contains(field, w.opts.Delimiter) || strings.ContainsRune(field, w.opts.QuoteChar)
-  default:
-    panic("Unexpected quoting.")
-  }
+	switch w.opts.Quoting {
+	case QuoteNone:
+		return false
+	case QuoteAll:
+		return true
+	case QuoteNonNumeric:
+		return !isNumeric(field)
+	case QuoteMinimal:
+		// TODO: Can be improved by making a single search with trie.
+		// See https://docs.python.org/2/library/csv.html#csv.QUOTE_MINIMAL for info on this.
+		return strings.Contains(field, w.opts.LineTerminator) || strings.Contains(field, w.opts.Delimiter) || strings.ContainsRune(field, w.opts.QuoteChar)
+	default:
+		panic("Unexpected quoting.")
+	}
 }
 
 func (w Writer) writeRune(r rune) error {
-  _, err := w.w.WriteRune(r)
-  return err
+	_, err := w.w.WriteRune(r)
+	return err
 }
 
 func (w Writer) writeEscapeChar(r rune) error {
-  switch w.opts.DoubleQuote {
-  case DoDoubleQuote:
-    return w.writeRune(r)
-  case NoDoubleQuote:
-    return w.writeRune(w.opts.EscapeChar)
-  default:
-    panic("Unrecognized double quote type.")
-  }
+	switch w.opts.DoubleQuote {
+	case DoDoubleQuote:
+		return w.writeRune(r)
+	case NoDoubleQuote:
+		return w.writeRune(w.opts.EscapeChar)
+	default:
+		panic("Unrecognized double quote type.")
+	}
 }
 
 func (w Writer) writeQuotedRune(r rune) error {
-  switch r {
-  case w.opts.EscapeChar:
-    if err := w.writeEscapeChar(r); err != nil {
-      return err
-    }
-  case w.opts.QuoteChar:
-    if err := w.writeEscapeChar(r); err != nil {
-      return err
-    }
-  }
-  return w.writeRune(r)
+	switch r {
+	case w.opts.EscapeChar:
+		if err := w.writeEscapeChar(r); err != nil {
+			return err
+		}
+	case w.opts.QuoteChar:
+		if err := w.writeEscapeChar(r); err != nil {
+			return err
+		}
+	}
+	return w.writeRune(r)
 }
 
 func (w Writer) writeQuoted(field string) error {
-  if err := w.writeRune(w.opts.QuoteChar); err != nil {
-    return err
-  }
-  for _, r := range field {
-    if err := w.writeQuotedRune(r); err != nil {
-      return err
-    }
-  }
-  return w.writeRune(w.opts.QuoteChar)
+	if err := w.writeRune(w.opts.QuoteChar); err != nil {
+		return err
+	}
+	for _, r := range field {
+		if err := w.writeQuotedRune(r); err != nil {
+			return err
+		}
+	}
+	return w.writeRune(w.opts.QuoteChar)
 }
 
 func (w Writer) writeField(field string) error {
-  if w.fieldNeedsQuote(field) {
-    return w.writeQuoted(field)
-  } else {
-    return w.writeString(field)
-  }
+	if w.fieldNeedsQuote(field) {
+		return w.writeQuoted(field)
+	} else {
+		return w.writeString(field)
+	}
 }
 
 func (w Writer) writeNewline() error {
-  return w.writeString(w.opts.LineTerminator)
+	return w.writeString(w.opts.LineTerminator)
 }
 
 // Writer writes a single CSV record to w along with any necessary quoting.
 // A record is a slice of strings with each string being one field.
 func (w Writer) Write(record []string) (err error) {
-  for n, field := range record {
-    if n > 0 {
-      if err = w.writeDelimiter(); err != nil {
-        return
-      }
-    }
-    if err = w.writeField(field); err != nil {
-      return
-    }
-  }
-  err = w.writeNewline()
-  return
+	for n, field := range record {
+		if n > 0 {
+			if err = w.writeDelimiter(); err != nil {
+				return
+			}
+		}
+		if err = w.writeField(field); err != nil {
+			return
+		}
+	}
+	err = w.writeNewline()
+	return
 }
 
 // WriteAll writes multiple CSV records to w using Write and then calls Flush.
 func (w Writer) WriteAll(records [][]string) (err error) {
-  for _, record := range records {
-    if err := w.Write(record); err != nil {
-      return err
-    }
-  }
-  return w.w.Flush()
+	for _, record := range records {
+		if err := w.Write(record); err != nil {
+			return err
+		}
+	}
+	return w.w.Flush()
 }
--- csv_test.go
+++ csv_test.go
@@ -1,209 +1,209 @@
 // Copyright 2014 Jens Rantil. All rights reserved.  Use of this source code is
 // governed by a BSD-style license that can be found in the LICENSE file.
 
-package csv;
+package csv
 
 import (
-  "bytes"
-  "testing"
-  "testing/quick"
-  oldcsv "encoding/csv"
+	"bytes"
+	oldcsv "encoding/csv"
+	"testing"
+	"testing/quick"
 )
 
 func TestInterfaceAdherence(t *testing.T) {
-  t.Parallel()
+	t.Parallel()
 
-  var iface CsvWriter
-  iface = NewWriter(new(bytes.Buffer))
-  iface = NewDialectWriter(new(bytes.Buffer), Dialect{})
-  iface = oldcsv.NewWriter(new(bytes.Buffer))
+	var iface CsvWriter
+	iface = NewWriter(new(bytes.Buffer))
+	iface = NewDialectWriter(new(bytes.Buffer), Dialect{})
+	iface = oldcsv.NewWriter(new(bytes.Buffer))
 
-  // To get rid of compile-time warning that this variable is not used.
-  iface.Flush()
+	// To get rid of compile-time warning that this variable is not used.
+	iface.Flush()
 }
 
 // Execute a quicktest for a specific quoting.
 func testQuick(t *testing.T, quoting int) {
-  f := func(records [][]string, doubleQuote bool, escapeChar, quoteChar rune, del, lt string) bool {
-    b1 := new(bytes.Buffer)
-    dialect := Dialect{
-      Quoting: quoting,
-      EscapeChar: escapeChar,
-      QuoteChar: quoteChar,
-      Delimiter: del,
-      LineTerminator: lt,
-    }
-    if doubleQuote {
-      dialect.DoubleQuote = DoDoubleQuote
-    } else {
-      dialect.DoubleQuote = NoDoubleQuote
-    }
-    w := NewDialectWriter(b1, dialect)
-    for _, record := range records {
-      w.Write(record)
-    }
-    w.Flush()
+	f := func(records [][]string, doubleQuote bool, escapeChar, quoteChar rune, del, lt string) bool {
+		b1 := new(bytes.Buffer)
+		dialect := Dialect{
+			Quoting:        quoting,
+			EscapeChar:     escapeChar,
+			QuoteChar:      quoteChar,
+			Delimiter:      del,
+			LineTerminator: lt,
+		}
+		if doubleQuote {
+			dialect.DoubleQuote = DoDoubleQuote
+		} else {
+			dialect.DoubleQuote = NoDoubleQuote
+		}
+		w := NewDialectWriter(b1, dialect)
+		for _, record := range records {
+			w.Write(record)
+		}
+		w.Flush()
 
-    b2 := new(bytes.Buffer)
-    w = NewDialectWriter(b2, dialect)
-    w.WriteAll(records)
+		b2 := new(bytes.Buffer)
+		w = NewDialectWriter(b2, dialect)
+		w.WriteAll(records)
 
-    return bytes.Equal(b1.Bytes(), b2.Bytes())
-  }
-  if err := quick.Check(f, nil); err != nil {
-    t.Error(err)
-  }
+		return bytes.Equal(b1.Bytes(), b2.Bytes())
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
 }
 
 // Run quicktest using various quoting types
 func TestQuick(t *testing.T) {
-  t.Parallel()
+	t.Parallel()
 
-  testQuick(t, QuoteAll)
-  testQuick(t, QuoteNone)
-  testQuick(t, QuoteMinimal)
-  testQuick(t, QuoteNonNumeric)
+	testQuick(t, QuoteAll)
+	testQuick(t, QuoteNone)
+	testQuick(t, QuoteMinimal)
+	testQuick(t, QuoteNonNumeric)
 }
 
 func TestBasic(t *testing.T) {
-  t.Parallel()
+	t.Parallel()
 
-  b := new(bytes.Buffer)
-  w := NewWriter(b)
-  w.Write([]string{
-    "a",
-    "b",
-    "c",
-  })
-  w.Flush()
-  if s := string(b.Bytes()); s != "a b c\n" {
-    t.Error("Unexpected output:", s)
-  }
+	b := new(bytes.Buffer)
+	w := NewWriter(b)
+	w.Write([]string{
+		"a",
+		"b",
+		"c",
+	})
+	w.Flush()
+	if s := string(b.Bytes()); s != "a b c\n" {
+		t.Error("Unexpected output:", s)
+	}
 
-  w.Write([]string{
-    "d",
-    "e",
-    "f",
-  })
-  w.Flush()
-  if s := string(b.Bytes()); s != "a b c\nd e f\n" {
-    t.Error("Unexpected output:", s)
-  }
+	w.Write([]string{
+		"d",
+		"e",
+		"f",
+	})
+	w.Flush()
+	if s := string(b.Bytes()); s != "a b c\nd e f\n" {
+		t.Error("Unexpected output:", s)
+	}
 }
 
 func TestMinimalQuoting(t *testing.T) {
-  t.Parallel()
+	t.Parallel()
 
-  b := new(bytes.Buffer)
-  w := NewWriter(b)
+	b := new(bytes.Buffer)
+	w := NewWriter(b)
 
-  if w.opts.Quoting != QuoteMinimal {
-    t.Fatal("Unexpected quoting.")
-  }
-  if s := "b c"; !w.fieldNeedsQuote(s) {
-    t.Error("Expected field to need quoting:", s)
-  }
+	if w.opts.Quoting != QuoteMinimal {
+		t.Fatal("Unexpected quoting.")
+	}
+	if s := "b c"; !w.fieldNeedsQuote(s) {
+		t.Error("Expected field to need quoting:", s)
+	}
 
-  w.Write([]string{
-    "a",
-    "b c",
-    "d",
-  })
-  w.Flush()
-  if s := string(b.Bytes()); s != "a \"b c\" d\n" {
-    t.Error("Unexpected output:", s)
-  }
+	w.Write([]string{
+		"a",
+		"b c",
+		"d",
+	})
+	w.Flush()
+	if s := string(b.Bytes()); s != "a \"b c\" d\n" {
+		t.Error("Unexpected output:", s)
+	}
 }
 
 func TestIsNumeric(t *testing.T) {
-  t.Parallel()
+	t.Parallel()
 
-  notNumeric := []string{
-    "",
-    " ",
-    "a",
-    "1a",
-    "a1",
-  }
-  numeric := []string{
-    "1",
-    "11",
-    "123456789",
-  }
-  for _, item := range numeric {
-    if !isNumeric(item) {
-      t.Error("Should be numeric:", item)
-    }
-  }
-  for _, item := range notNumeric {
-    if isNumeric(item) {
-      t.Error("Should not be numeric:", item)
-    }
-  }
+	notNumeric := []string{
+		"",
+		" ",
+		"a",
+		"1a",
+		"a1",
+	}
+	numeric := []string{
+		"1",
+		"11",
+		"123456789",
+	}
+	for _, item := range numeric {
+		if !isNumeric(item) {
+			t.Error("Should be numeric:", item)
+		}
+	}
+	for _, item := range notNumeric {
+		if isNumeric(item) {
+			t.Error("Should not be numeric:", item)
+		}
+	}
 }
 
 func TestNumericQuoting(t *testing.T) {
-  t.Parallel()
+	t.Parallel()
 
-  b := new(bytes.Buffer)
-  dialect := Dialect{
-    Quoting: QuoteNonNumeric,
-  }
-  w := NewDialectWriter(b, dialect)
-  w.Write([]string{
-    "a",
-    "112",
-    "b c",
-  })
-  w.Flush()
-  if s := string(b.Bytes()); s != "\"a\" 112 \"b c\"\n" {
-    t.Error("Unexpected output:", s)
-  }
+	b := new(bytes.Buffer)
+	dialect := Dialect{
+		Quoting: QuoteNonNumeric,
+	}
+	w := NewDialectWriter(b, dialect)
+	w.Write([]string{
+		"a",
+		"112",
+		"b c",
+	})
+	w.Flush()
+	if s := string(b.Bytes()); s != "\"a\" 112 \"b c\"\n" {
+		t.Error("Unexpected output:", s)
+	}
 }
 
 func TestEscaping(t *testing.T) {
-  t.Parallel()
+	t.Parallel()
 
-  b := new(bytes.Buffer)
-  w := NewWriter(b)
-  w.Write([]string{
-    "a",
-    "\"",
-    "b c",
-  })
-  w.Flush()
-  if s := string(b.Bytes()); s != "a \"\"\"\" \"b c\"\n" {
-    t.Error("Unexpected output:", s)
-  }
+	b := new(bytes.Buffer)
+	w := NewWriter(b)
+	w.Write([]string{
+		"a",
+		"\"",
+		"b c",
+	})
+	w.Flush()
+	if s := string(b.Bytes()); s != "a \"\"\"\" \"b c\"\n" {
+		t.Error("Unexpected output:", s)
+	}
 
-  b.Reset()
-  dialect := Dialect{
-    DoubleQuote: NoDoubleQuote,
-  }
-  w = NewDialectWriter(b, dialect)
-  w.Write([]string{
-    "a",
-    "\"",
-    "b c",
-  })
-  w.Flush()
-  if s := string(b.Bytes()); s != "a \"\\\"\" \"b c\"\n" {
-    t.Error("Unexpected output:", s)
-  }
+	b.Reset()
+	dialect := Dialect{
+		DoubleQuote: NoDoubleQuote,
+	}
+	w = NewDialectWriter(b, dialect)
+	w.Write([]string{
+		"a",
+		"\"",
+		"b c",
+	})
+	w.Flush()
+	if s := string(b.Bytes()); s != "a \"\\\"\" \"b c\"\n" {
+		t.Error("Unexpected output:", s)
+	}
 }
 
 func TestNewLineRecord(t *testing.T) {
-  t.Parallel()
+	t.Parallel()
 
-  b := new(bytes.Buffer)
-  w := NewWriter(b)
-  w.Write([]string{
-    "a",
-    "he\nllo",
-    "b c",
-  })
-  w.Flush()
-  if s := string(b.Bytes()); s != "a \"he\nllo\" \"b c\"\n" {
-    t.Error("Unexpected output:", s)
-  }
+	b := new(bytes.Buffer)
+	w := NewWriter(b)
+	w.Write([]string{
+		"a",
+		"he\nllo",
+		"b c",
+	})
+	w.Flush()
+	if s := string(b.Bytes()); s != "a \"he\nllo\" \"b c\"\n" {
+		t.Error("Unexpected output:", s)
+	}
 }
