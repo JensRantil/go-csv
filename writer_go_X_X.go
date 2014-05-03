5b43c0f0662f5d7b4a0d8c9e02d8dce031f2ab2d
Jens Rantil <jens.rantil@gmail.com>
2014-05-03 21:55:17 +0200

    docs: add missing license headers in source

--- writer_go_X_X.go
+++ writer_go_X_X.go
@@ -1,3 +1,6 @@
+// Copyright 2014 Jens Rantil. All rights reserved.  Use of this source code is
+// governed by a BSD-style license that can be found in the LICENSE file.
+
 // +build go1.1
 
 package csv
